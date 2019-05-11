d6d3eef43502b3393429466116e0078899cd4b80
Mr.Singla <mr.Singla@Ashwanis-MacBook-Pro.local>
2019-05-12 03:03:51 +0530

    facebook url redirection done

--- server/passport.js
+++ server/passport.js
@@ -1,40 +1,62 @@
 import express from 'express';
+import User from './models/user.model';
+import jwt from 'jsonwebtoken';
+import config from './../config/config'
 var passport = require('passport');
 const router = express.Router();
+var token = "";
 
 var FacebookStrategy = require('passport-facebook').Strategy;
-var User = require('./models/user.model');
 
-    passport.serializeUser(function (user, cb) {
+passport.serializeUser(function (user, cb) {
+    console.log("User in serialize:" + user._id);
+
+     token = jwt.sign({
+        _id: user._id
+    }, config.jwtSecret);
+    console.log("Token generated" + token);
+    cb(null, user);
+});
+
+passport.deserializeUser(function (id, cb) {
+    console.log("Obj id at deserialize:" + id);
+    User.findById(id,function(err,user){
         cb(null, user);
     });
-
-    passport.deserializeUser(function (obj, cb) {
-        cb(null, obj);
-    });
+});
 
 
 
-    passport.use(new FacebookStrategy({
-        clientID: '403978703755010',
-        clientSecret: '9aa31c25226244b0a8053023bfea2d0b',
-        callbackURL: "http://localhost:3000/auth/facebook/callback",
-        profileFields: ['id', 'displayName', 'photos', 'email']
-    },
-        function (accessToken, refreshToken, profile, cb) {
-            console.log(profile._json.email);
-            cb(null, profile);
-        }
-    ));
+passport.use(new FacebookStrategy({
+    clientID: '403978703755010',
+    clientSecret: '9aa31c25226244b0a8053023bfea2d0b',
+    callbackURL: "http://localhost:3000/auth/facebook/callback",
+    profileFields: ['id', 'displayName', 'photos', 'email']
+},
+    function (accessToken, refreshToken, profile, cb) {
+        console.log(profile._json.email);
 
-    router.route('/auth/facebook')
+        User.findOne({ email: profile._json.email }).select('password email').exec(function(err, user) {
+            if (err) done(err);
+            if (user && user !== null) {
+                console.log("user found in middleware:" + user);
+                cb(null, user);
+            } else {
+                cb(err);
+            }
+        });
+        console.log("User not found in middleware");
+    }
+));
+
+router.route('/auth/facebook')
     .get(passport.authenticate('facebook', { scope: 'email' }));
 
-    router.route('/auth/facebook/callback')
+router.route('/auth/facebook/callback')
     .get(passport.authenticate('facebook', { failureRedirect: '/login' }),
         function (req, res) {
-            // Successful authentication, redirect home.
-            res.redirect('/');
+            console.log("Reached at success level" + token);
+            res.redirect('/facebook/' + token);
         });
 
-   export default router;
+export default router;
